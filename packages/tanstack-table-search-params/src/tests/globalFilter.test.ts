6355ac7c796c437656e991e3971428364d5e5f37
taro-28 <tarotaro0208@gmail.com>
2024-07-15 19:37:56 +0900

    Add integration tests

---
+++ packages/tanstack-table-search-params/src/tests/globalFilter.test.ts
@@ -0,0 +1,33 @@
+import { act } from "@testing-library/react";
+import mockRouter from "next-router-mock";
+import { expect, test } from "vitest";
+import { useTableSearchParams } from "..";
+import { getCoreRowModel, useReactTable } from "@tanstack/react-table";
+import { renderHook } from "@testing-library/react";
+
+test("globalFilter", () => {
+  const { result, rerender } = renderHook(() => {
+    const stateAndOnChanges = useTableSearchParams(mockRouter);
+    return useReactTable({
+      columns: [],
+      data: [],
+      getCoreRowModel: getCoreRowModel(),
+      ...stateAndOnChanges,
+    });
+  });
+
+  expect(result.current.getState().globalFilter).toBe("");
+  expect(mockRouter.query).toEqual({});
+
+  act(() => result.current.setGlobalFilter("John"));
+  rerender();
+
+  expect(result.current.getState().globalFilter).toBe("John");
+  expect(mockRouter.query).toEqual({ globalFilter: "John" });
+
+  act(() => result.current.setGlobalFilter(""));
+  rerender();
+
+  expect(result.current.getState().globalFilter).toBe("");
+  expect(mockRouter.query).toEqual({});
+});
---
+++ packages/tanstack-table-search-params/src/tests/sorting.test.ts
@@ -0,0 +1,144 @@
+import { act } from "@testing-library/react";
+import mockRouter from "next-router-mock";
+import { describe, expect, test } from "vitest";
+import { useTableSearchParams } from "..";
+import { getCoreRowModel, useReactTable } from "@tanstack/react-table";
+import { renderHook } from "@testing-library/react";
+
+describe("sorting", () => {
+  test("single column", () => {
+    const { result, rerender } = renderHook(() => {
+      const stateAndOnChanges = useTableSearchParams(mockRouter);
+      return useReactTable({
+        columns: [{ accessorKey: "id" }, { accessorKey: "name" }],
+        data: [
+          { id: 0, name: "John" },
+          { id: 1, name: "Jane" },
+        ],
+        getCoreRowModel: getCoreRowModel(),
+        ...stateAndOnChanges,
+      });
+    });
+
+    expect(result.current.getState().sorting).toEqual([]);
+    expect(mockRouter.query).toEqual({});
+
+    // sort by first column
+    act(() =>
+      result.current.getFlatHeaders()[0]?.column.getToggleSortingHandler()?.(
+        {},
+      ),
+    );
+    rerender();
+
+    expect(result.current.getState().sorting).toEqual([
+      { id: "id", desc: true },
+    ]);
+    expect(mockRouter.query).toEqual({ sorting: "id.desc" });
+
+    // sort by first column again
+    act(() =>
+      result.current.getFlatHeaders()[0]?.column.getToggleSortingHandler()?.(
+        {},
+      ),
+    );
+    rerender();
+
+    expect(result.current.getState().sorting).toEqual([
+      { id: "id", desc: false },
+    ]);
+    expect(mockRouter.query).toEqual({ sorting: "id.asc" });
+
+    // sorted by another column
+    act(() =>
+      result.current.getFlatHeaders()[1]?.column.getToggleSortingHandler()?.(
+        {},
+      ),
+    );
+    rerender();
+
+    expect(result.current.getState().sorting).toEqual([
+      { id: "name", desc: false },
+    ]);
+    expect(mockRouter.query).toEqual({ sorting: "name.asc" });
+
+    // reset
+    act(() => {
+      result.current.getFlatHeaders()[1]?.column.getToggleSortingHandler()?.(
+        {},
+      );
+    });
+    rerender();
+    act(() => {
+      result.current.getFlatHeaders()[1]?.column.getToggleSortingHandler()?.(
+        {},
+      );
+    });
+    rerender();
+
+    expect(result.current.getState().sorting).toEqual([]);
+    expect(mockRouter.query).toEqual({});
+  });
+
+  test("multiple columns", () => {
+    const { result, rerender } = renderHook(() => {
+      const stateAndOnChanges = useTableSearchParams(mockRouter);
+      return useReactTable({
+        columns: [{ accessorKey: "id" }, { accessorKey: "name" }],
+        data: [
+          { id: 0, name: "John" },
+          { id: 1, name: "Jane" },
+        ],
+        getCoreRowModel: getCoreRowModel(),
+        ...stateAndOnChanges,
+      });
+    });
+
+    expect(result.current.getState().sorting).toEqual([]);
+    expect(mockRouter.query).toEqual({});
+
+    act(() =>
+      result.current.getFlatHeaders()[0]?.column.getToggleSortingHandler()?.(
+        {},
+      ),
+    );
+    rerender();
+    act(() =>
+      result.current.getFlatHeaders()[1]?.column.getToggleSortingHandler()?.({
+        shiftKey: true,
+      }),
+    );
+    rerender();
+
+    expect(result.current.getState().sorting).toEqual([
+      { id: "id", desc: true },
+      { id: "name", desc: false },
+    ]);
+    expect(mockRouter.query).toEqual({ sorting: "id.desc,name.asc" });
+
+    act(() =>
+      result.current.getFlatHeaders()[0]?.column.getToggleSortingHandler()?.({
+        shiftKey: true,
+      }),
+    );
+    rerender();
+
+    expect(result.current.getState().sorting).toEqual([
+      { id: "id", desc: false },
+      { id: "name", desc: false },
+    ]);
+    expect(mockRouter.query).toEqual({ sorting: "id.asc,name.asc" });
+
+    act(() =>
+      result.current.getFlatHeaders()[0]?.column.getToggleSortingHandler()?.(
+        {},
+      ),
+    );
+    rerender();
+
+    expect(result.current.getState().sorting).toEqual([
+      { id: "id", desc: false },
+    ]);
+    expect(mockRouter.query).toEqual({ sorting: "id.asc" });
+  });
+});
