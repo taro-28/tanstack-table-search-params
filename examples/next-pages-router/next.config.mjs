e2eb0ad9a34bb2d2a7b45eb40445b095a597f141
taro-28 <tarotaro0208@gmail.com>
2024-07-20 17:34:05 +0900

    Update tanstack-table-search-params dependency to use workspace link

--- examples/next-pages-router/next.config.mjs
+++ examples/next-pages-router/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ["@tanstack-table-search-params/next"],
+  transpilePackages: ["tanstack-table-search-params"],
 };
 
 export default nextConfig;
