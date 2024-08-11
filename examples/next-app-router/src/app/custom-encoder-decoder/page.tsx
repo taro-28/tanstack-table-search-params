10c25c92bae3a79401f0634ccb31405af8225a02
taro-28 <tarotaro0208@gmail.com>
2024-08-11 21:51:35 +0900

    chore: Add "dist" folder to build outputs in turbo.json

--- examples/next-app-router/src/app/custom-encoder-decoder/page.tsx
+++ examples/next-app-router/src/app/custom-encoder-decoder/page.tsx
@@ -1,10 +1,13 @@
+import { Suspense } from "react";
 import { Table } from "./table";
 
 export default function Page() {
   return (
     <main className="space-y-2 mx-6">
       <h1 className="text-lg font-semibold">Custom encoder/decoder</h1>
-      <Table />
+      <Suspense>
+        <Table />
+      </Suspense>
     </main>
   );
 }
--- examples/next-app-router/src/app/page.tsx
+++ examples/next-app-router/src/app/page.tsx
@@ -1,10 +1,13 @@
+import { Suspense } from "react";
 import { BasicTable } from "./table";
 
 export default function Page() {
   return (
     <main className="space-y-2 mx-6">
       <h1 className="text-lg font-semibold">Basic</h1>
-      <BasicTable />
+      <Suspense>
+        <BasicTable />
+      </Suspense>
     </main>
   );
 }
