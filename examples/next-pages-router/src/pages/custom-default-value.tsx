9f7f296f35e4fce93f92f090f30b7ce1a946bffc
taro-28 <tarotaro0208@gmail.com>
2024-08-28 13:16:17 +0900

    Add examples for default value option

---
+++ examples/next-pages-router/src/pages/custom-default-value.tsx
@@ -0,0 +1,43 @@
+import { UserTable } from "@examples/lib/src/components/UserTable";
+import {
+  useUserData,
+  userColumns,
+} from "@examples/lib/src/components/userData";
+import {
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import { useRouter } from "next/router";
+import { useTableSearchParams } from "tanstack-table-search-params";
+
+export default function CustomParamNames() {
+  const data = useUserData();
+
+  const router = useRouter();
+  const stateAndOnChanges = useTableSearchParams(router, {
+    globalFilter: { defaultValue: "a" },
+    sorting: { defaultValue: [{ id: "name", desc: true }] },
+    pagination: { defaultValue: { pageIndex: 2, pageSize: 20 } },
+    columnFilters: { defaultValue: [{ id: "name", value: "b" }] },
+  });
+
+  const table = useReactTable({
+    data,
+    columns: userColumns,
+    getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    ...stateAndOnChanges,
+  });
+
+  return (
+    <div className="space-y-2 mx-6">
+      <h1 className="text-lg font-semibold">Custom default value</h1>
+      <UserTable table={table} />
+    </div>
+  );
+}
