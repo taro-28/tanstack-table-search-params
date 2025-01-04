0a28632efc50ca9b5b6eaf1ed0dd245c21f20387
taro-28 <tarotaro0208@gmail.com>
2025-01-04 12:58:30 +0900

    Fix warning

--- examples/lib/src/components/SearchInput.tsx
+++ examples/lib/src/components/SearchInput.tsx
@@ -1,22 +1,23 @@
 import type React from "react";
 import { type ChangeEvent, useRef, useState } from "react";
 
-export const SearchInput = (
-  props: Omit<React.JSX.IntrinsicElements["input"], "onChange"> & {
-    onSearch: (value: string) => void;
-  },
-) => {
+export const SearchInput = ({
+  onSearch,
+  ...props
+}: Omit<React.JSX.IntrinsicElements["input"], "onChange"> & {
+  onSearch: (value: string) => void;
+}) => {
   const ref = useRef<HTMLInputElement>(null);
   const [isComposing, setIsComposing] = useState(false);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     if (isComposing) return;
-    props.onSearch(e.target.value);
+    onSearch(e.target.value);
   };
   const handleCompositionStart = () => setIsComposing(true);
   const handleCompositionEnd = () => {
     setIsComposing(false);
-    props.onSearch(ref.current?.value ?? "");
+    onSearch(ref.current?.value ?? "");
   };
   return (
     <input
